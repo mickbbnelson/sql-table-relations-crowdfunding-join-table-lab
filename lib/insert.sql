23d206c88bf72f9cfa16240f2d7b6f3a25e21d43
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-04-27 15:31:38 -0700

    Done but not done, Results match but still showing red

---
+++ lib/insert.sql
@@ -0,0 +1,68 @@
+INSERT INTO users VALUES (1, "Chaz", 28);
+INSERT INTO users VALUES (2, "Jerry", 27);
+INSERT INTO users VALUES (3, "Bobby", 29);
+INSERT INTO users VALUES (4, "Phil", 26);
+INSERT INTO users VALUES (5, "Bill", 30);
+INSERT INTO users VALUES (6, "Mickey", 25);
+INSERT INTO users VALUES (7, "Ron", 40);
+INSERT INTO users VALUES (8, "Kieth", 41);
+INSERT INTO users VALUES (9, "Donna", 39);
+INSERT INTO users VALUES (10, "Brent", 42);
+INSERT INTO users VALUES (11, "Sheri", 38);
+INSERT INTO users VALUES (12, "Shannon", 43);
+INSERT INTO users VALUES (13, "Kerry", 37);
+INSERT INTO users VALUES (14, "Lucy", 44);
+INSERT INTO users VALUES (15, "Ashley", 36);
+INSERT INTO users VALUES (16, "Lisa", 45);
+INSERT INTO users VALUES (17, "Laura", 35);
+INSERT INTO users VALUES (18, "Sarah", 46);
+INSERT INTO users VALUES (19, "Debby", 34);
+INSERT INTO users VALUES (20, "Mary", 47);
+
+INSERT INTO projects VALUES (1, 'Build Cabinets', 'Kitchen', $300, '2021-06-30', '2021-07-30');
+INSERT INTO projects VALUES (2, 'Fix Roof', 'Home', $400, '2021-07-30', '2013-08-30');
+INSERT INTO projects VALUES (3, 'Fix Grill', 'Yard', $200, '2021-08-30', '2021-09-30');
+INSERT INTO projects VALUES (4, 'Build Fence', 'Yard', $500, '2021-09-30', '2021-10-30');
+INSERT INTO projects VALUES (5, 'Furnish Office', 'Home', $600, '2021-10-30', '2021-11-30');
+INSERT INTO projects VALUES (6, 'Sod Grass', 'Yard', $100, '2021-11-30', '2021-12-30');
+INSERT INTO projects VALUES (7, 'Tile', 'Kitchen', $700, '2022-01-30', '2022-02-30');
+INSERT INTO projects VALUES (8, 'Dish Washer', 'Kitchen', $800, '2022-02-30', '2022-03-30');
+INSERT INTO projects VALUES (9, 'Couch', 'Home', $900, '2022-04-30', '2022-05-30');
+INSERT INTO projects VALUES (10, 'Volleyball Court', 'Yard', $1000, '2022-05-30', '2022-06-30');
+
+INSERT INTO pledges VALUES (1, 25, 20, 10);
+INSERT INTO pledges VALUES (2, 30, 19, 9);
+INSERT INTO pledges VALUES (3, 35, 18, 8);
+INSERT INTO pledges VALUES (4, 40, 17, 7);
+INSERT INTO pledges VALUES (5, 45, 16, 6);
+INSERT INTO pledges VALUES (6, 50, 15, 5);
+INSERT INTO pledges VALUES (7, 55, 14, 4);
+INSERT INTO pledges VALUES (8, 60, 13, 3);
+INSERT INTO pledges VALUES (9, 65, 12, 2);
+INSERT INTO pledges VALUES (10, 70, 11, 1);
+INSERT INTO pledges VALUES (11, 75, 10, 1);
+INSERT INTO pledges VALUES (12, 80, 9, 2);
+INSERT INTO pledges VALUES (13, 85, 8, 3);
+INSERT INTO pledges VALUES (14, 90, 7, 4);
+INSERT INTO pledges VALUES (15, 95, 6, 5);
+INSERT INTO pledges VALUES (16, 100, 5, 6);
+INSERT INTO pledges VALUES (17, 105, 4, 7);
+INSERT INTO pledges VALUES (18, 110, 3, 8);
+INSERT INTO pledges VALUES (19, 115, 2, 9);
+INSERT INTO pledges VALUES (20, 120, 1, 10);
+INSERT INTO pledges VALUES (21, 125, 1, 10);
+INSERT INTO pledges VALUES (22, 130, 2, 9);
+INSERT INTO pledges VALUES (23, 135, 3, 8);
+INSERT INTO pledges VALUES (24, 140, 4, 7);
+INSERT INTO pledges VALUES (25, 145, 5, 6);
+INSERT INTO pledges VALUES (26, 150, 6, 5);
+INSERT INTO pledges VALUES (27, 155, 7, 4);
+INSERT INTO pledges VALUES (28, 160, 8, 3);
+INSERT INTO pledges VALUES (29, 165, 9, 2);
+INSERT INTO pledges VALUES (30, 170, 10, 1);
+
+
+
+
+
+
